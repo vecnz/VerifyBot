154d58ab5a9f0442ff3970714999981169099e08
NightFIsh <nightfish.net@gmail.com>
2022-03-13 04:12:37 +1300

    Clean up imports.

--- src/VerifyBot/VerifyBot/Services/DiscordBot/DiscordBot.cs
+++ src/VerifyBot/VerifyBot/Services/DiscordBot/DiscordBot.cs
@@ -1,15 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Discord;
-using Discord.Net;
 using Discord.WebSocket;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using VerifyBot.Services.DiscordBot.Configuration;
 
 namespace VerifyBot.Services.DiscordBot
