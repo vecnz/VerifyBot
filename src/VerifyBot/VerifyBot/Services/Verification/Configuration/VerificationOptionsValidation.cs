341a07e84f3a2db21f4522923ac41f8897d1fc4f
NightFIsh <nightfish.net@gmail.com>
2022-03-14 00:52:28 +1300

    Add validation for verification options.

--- src/VerifyBot/VerifyBot/Services/Verification/Configuration/VerificationOptionsValidation.cs
+++ src/VerifyBot/VerifyBot/Services/Verification/Configuration/VerificationOptionsValidation.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using Microsoft.Extensions.Options;
 
 namespace VerifyBot.Services.Verification.Configuration
@@ -6,6 +9,32 @@ namespace VerifyBot.Services.Verification.Configuration
     {
         public ValidateOptionsResult Validate(string name, VerificationOptions options)
         {
+            // Validate email regex.
+            if (string.IsNullOrWhiteSpace(options.EmailPattern))
+            {
+                return ValidateOptionsResult.Fail("Missing email RegEx pattern.");
+            }
+            
+            try
+            {
+                Regex.Match("", options.EmailPattern);
+            }
+            catch (ArgumentException)
+            {
+                return ValidateOptionsResult.Fail("Invalid email RegEx pattern.");
+            }
+
+            // Validate public key path.
+            if (string.IsNullOrWhiteSpace(options.PublicKeyPath))
+            {
+                return ValidateOptionsResult.Fail("Missing public key path.");
+            }
+            
+            if (!File.Exists(options.PublicKeyPath))
+            {
+                return ValidateOptionsResult.Fail($"File not found: \"{options.PublicKeyPath}\"");
+            }
+            
             return ValidateOptionsResult.Success;
         }
     }
