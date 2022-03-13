8d2d45989d13fbe0cf5b724ef83efb6e67f338cf
NightFIsh <nightfish.net@gmail.com>
2022-03-13 23:52:09 +1300

    Add MySql connection string checking in options validation.

--- src/VerifyBot/VerifyBot/Services/Storage/MySql/Configuration/MySqlStorageOptionsValidation.cs
+++ src/VerifyBot/VerifyBot/Services/Storage/MySql/Configuration/MySqlStorageOptionsValidation.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Data;
 using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
 
 namespace VerifyBot.Services.Storage.MySql.Configuration
 {
@@ -6,9 +9,32 @@ namespace VerifyBot.Services.Storage.MySql.Configuration
     {
         public ValidateOptionsResult Validate(string name, MySqlStorageOptions options)
         {
-            if (string.IsNullOrWhiteSpace(options.ConnectionString))
+            MySqlConnection con = null;
+            try
             {
-                return ValidateOptionsResult.Fail("Missing MySql connection string.");
+                con = new MySqlConnection(options.ConnectionString);
+                con.Open();
+            }
+            catch (ArgumentException ex)
+            {
+                return ValidateOptionsResult.Fail($"Invalid MySql connection string. {ex.Message}");
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    //http://dev.mysql.com/doc/refman/5.0/en/error-messages-server.html
+                    case 1042: // Unable to connect to any of the specified MySQL hosts (Check Server,Port)
+                        return ValidateOptionsResult.Fail($"Failed to connect to MySql DB host. {ex.Message}");
+                    case 0: // Access denied (Check DB name,username,password)
+                        return ValidateOptionsResult.Fail($"Failed to connect to MySql DB. Access denied. {ex.Message}");
+                    default:
+                        return ValidateOptionsResult.Fail($"Failed to connect to MySql DB. {ex.Message}");
+                }
+            }
+            finally
+            {
+                if (con?.State == ConnectionState.Open) con.Close();
             }
 
             return ValidateOptionsResult.Success;
