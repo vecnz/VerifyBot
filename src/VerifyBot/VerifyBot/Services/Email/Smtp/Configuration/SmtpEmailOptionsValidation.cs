5280af116117d43205dc712feb892d238532e265
NightFIsh <nightfish.net@gmail.com>
2022-03-15 21:46:03 +1300

    Add some validation for SMTP options.

--- src/VerifyBot/VerifyBot/Services/Email/Smtp/Configuration/SmtpEmailOptionsValidation.cs
+++ src/VerifyBot/VerifyBot/Services/Email/Smtp/Configuration/SmtpEmailOptionsValidation.cs
@@ -11,7 +11,36 @@ namespace VerifyBot.Services.Email.Smtp.Configuration
     {
         public ValidateOptionsResult Validate(string name, SmtpEmailOptions options)
         {
-            
+            if (string.IsNullOrWhiteSpace(options.Host))
+            {
+                return ValidateOptionsResult.Fail("Missing SMTP host");
+            }
+
+            if (options.Port < 0)
+            {
+                return ValidateOptionsResult.Fail("SMTP port cannot be negative.");
+            }
+        
+            if (string.IsNullOrWhiteSpace(options.FromAddress))
+            {
+                return ValidateOptionsResult.Fail("Missing SMTP from address.");
+            }
+        
+            if (string.IsNullOrWhiteSpace(options.FromName))
+            {
+                return ValidateOptionsResult.Fail("Missing SMTP from name.");
+            }
+        
+            if (string.IsNullOrWhiteSpace(options.SubjectTemplate))
+            {
+                return ValidateOptionsResult.Fail("Missing SMTP subject template.");
+            }
+        
+            if (string.IsNullOrWhiteSpace(options.BodyTemplate))
+            {
+                return ValidateOptionsResult.Fail("Missing SMTP body template.");
+            }
+
             return ValidateOptionsResult.Success;
         }
     }
