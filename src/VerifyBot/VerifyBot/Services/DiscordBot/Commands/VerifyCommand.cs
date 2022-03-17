42c13abbe5f566de48a34a5089a6769ecc83eb3e
NightFIsh <nightfish.net@gmail.com>
2022-03-17 21:15:16 +1300

    Move verification using token to separate command.

--- src/VerifyBot/VerifyBot/Services/DiscordBot/Commands/VerifyCommand.cs
+++ src/VerifyBot/VerifyBot/Services/DiscordBot/Commands/VerifyCommand.cs
@@ -11,7 +11,7 @@ namespace VerifyBot.Services.DiscordBot.Commands
     {
         private readonly VerificationService _verificationService;
         private readonly ITranslator _translator;
-        
+
         public VerifyCommand(VerificationService verificationService, ITranslator translator)
         {
             _verificationService = verificationService ?? throw new ArgumentNullException(nameof(verificationService));
@@ -26,9 +26,9 @@ namespace VerifyBot.Services.DiscordBot.Commands
                 .WithName(_translator.T("VERIFY_COMMAND_NAME"))
                 .WithDescription(_translator.T("VERIFY_COMMAND_DESCRIPTION"))
                 .AddOption(
-                    _translator.T("VERIFY_COMMAND_EMAIL_TOKEN_OPTION_NAME"),
+                    _translator.T("VERIFY_COMMAND_EMAIL_OPTION_NAME"),
                     ApplicationCommandOptionType.String,
-                    _translator.T("VERIFY_COMMAND_EMAIL_TOKEN_OPTION_DESCRIPTION"),
+                    _translator.T("VERIFY_COMMAND_EMAIL_OPTION_DESCRIPTION"),
                     true)
                 .Build();
         }
@@ -36,52 +36,26 @@ namespace VerifyBot.Services.DiscordBot.Commands
         public async Task ExecuteAsync(ISlashCommandInteraction command)
         {
             string firstOption = command.Data.Options.First().Value.ToString();
-            if (VerificationService.IsVerificationToken(firstOption))
+            VerificationService.StartVerificationResult result =
+                await _verificationService.StartVerificationAsync(command.User.Id, firstOption);
+            switch (result)
             {
-                VerificationService.FinishVerificationResult result =
-                    await _verificationService.FinishVerificationAsync(command.User.Id, firstOption);
-                switch (result)
-                {
-                    case VerificationService.FinishVerificationResult.Failure:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_FINISH_FAIL"), ephemeral: true);
-                        break;
-                    case VerificationService.FinishVerificationResult.InvalidToken:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_FINISH_INVALID_TOKEN"), ephemeral: true);
-                        break;
-                    case VerificationService.FinishVerificationResult.TokenExpired:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_FINISH_EXPIRED_TOKEN"), ephemeral: true);
-                        break;
-                    case VerificationService.FinishVerificationResult.AlreadyVerified:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_FINISH_ALREADY_VERIFIED"), ephemeral: true);
-                        break;
-                    case VerificationService.FinishVerificationResult.Success:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_FINISH_SUCCESS"), ephemeral: true);
-                        break;
-                    default: // Should never hit this
-                        throw new ArgumentOutOfRangeException($"Unhandled {nameof(VerificationService.FinishVerificationResult)} case. " + result.ToString());
-                }
-            }
-            else
-            {
-                VerificationService.StartVerificationResult result =
-                    await _verificationService.StartVerificationAsync(command.User.Id, firstOption);
-                switch (result)
-                {
-                    case VerificationService.StartVerificationResult.Failure:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_START_FAIL"), ephemeral: true);
-                        break;
-                    case VerificationService.StartVerificationResult.InvalidEmail:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_START_INVALID_EMAIL"), ephemeral: true);
-                        break;
-                    case VerificationService.StartVerificationResult.AlreadyVerified:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_START_ALREADY_VERIFIED"), ephemeral: true);
-                        break;
-                    case VerificationService.StartVerificationResult.Success:
-                        await command.FollowupAsync(_translator.T("VERIFY_COMMAND_START_SUCCESS"), ephemeral: true);
-                        break;
-                    default: // Should never hit this
-                        throw new ArgumentOutOfRangeException($"Unhandled {nameof(VerificationService.StartVerificationResult)} case. " + result.ToString());
-                }
+                case VerificationService.StartVerificationResult.Failure:
+                    await command.FollowupAsync(_translator.T("VERIFY_COMMAND_FAIL"), ephemeral: true);
+                    break;
+                case VerificationService.StartVerificationResult.InvalidEmail:
+                    await command.FollowupAsync(_translator.T("VERIFY_COMMAND_INVALID_EMAIL"), ephemeral: true);
+                    break;
+                case VerificationService.StartVerificationResult.AlreadyVerified:
+                    await command.FollowupAsync(_translator.T("VERIFY_COMMAND_ALREADY_VERIFIED"),
+                        ephemeral: true);
+                    break;
+                case VerificationService.StartVerificationResult.Success:
+                    await command.FollowupAsync(_translator.T("VERIFY_COMMAND_SUCCESS"), ephemeral: true);
+                    break;
+                default: // Should never hit this
+                    throw new ArgumentOutOfRangeException(
+                        $"Unhandled {nameof(VerificationService.StartVerificationResult)} case. " + result.ToString());
             }
         }
     }
