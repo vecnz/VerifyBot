7f78e776fc136e5ff43c927b5d1ed29b53712aa1
Leon Bowie <leon@bowie-co.nz>
2022-08-30 21:29:58 +1200

    remove C#

--- src/VerifyBot/VerifyBot/Services/Translation/ITranslator.cs
+++
@@ -1,8 +0,0 @@
-namespace VerifyBot.Services.Translation
-{
-    public interface ITranslator
-    {
-        public string Translate(string key, params object[] values);
-        public string T(string key, params object[] values);
-    }
-}
