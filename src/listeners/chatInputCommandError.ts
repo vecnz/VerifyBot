58c31872b4c7705fb5023f3670d2717d76156dbc
Leon Bowie <leon@bowie-co.nz>
2022-08-30 21:52:02 +1200

    feat: add error handler

---
+++ src/listeners/chatInputCommandError.ts
@@ -0,0 +1,11 @@
+import { github } from '#lib/constants';
+import { Listener, ChatInputCommandErrorPayload } from '@sapphire/framework';
+
+export class ChatInputCommandError extends Listener {
+	public async run(err: Error, { interaction, command }: ChatInputCommandErrorPayload): Promise<void> {
+		this.container.logger.error(err);
+		await interaction.reply(
+			`An error occurred while executing the command \`${command.name}\`. If this keeps happening, please raise check our GitHub and raise an issue [here](${github}).`
+		);
+	}
+}
