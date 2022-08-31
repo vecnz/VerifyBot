92cbc5b6f931fdee8e72bc13e1e77a20685c9439
Leon Bowie <leon@bowie-co.nz>
2022-08-31 14:51:13 +1200

    fix: make error message ephemeral

--- src/listeners/chatInputCommandError.ts
+++ src/listeners/chatInputCommandError.ts
@@ -4,8 +4,9 @@ import { Listener, ChatInputCommandErrorPayload } from '@sapphire/framework';
 export class ChatInputCommandError extends Listener {
 	public async run(err: Error, { interaction, command }: ChatInputCommandErrorPayload): Promise<void> {
 		this.container.logger.error(err);
-		await interaction.reply(
-			`An error occurred while executing the command \`${command.name}\`. If this keeps happening, please raise check our GitHub and raise an issue [here](${github}).`
-		);
+		await interaction.reply({
+			content: `An error occurred while executing the command \`${command.name}\`. If this keeps happening, please raise check our GitHub and raise an issue [here](${github}).`,
+			ephemeral: true
+		});
 	}
 }
