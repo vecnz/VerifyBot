574269495b646d36aac939ab79aca383dc0be3e9
Leon Bowie <31436575+Darkflame72@users.noreply.github.com>
2022-10-11 12:11:43 +1300

    fix: update deps and fix typecheck (#57)

--- src/lib/Augements.d.ts
+++ src/lib/Augements.d.ts
@@ -1,9 +1,22 @@
 import type { PrismaClient } from '@prisma/client';
 import type { Transporter } from 'nodemailer';
+import type { IntegerString } from '@skyra/env-utilities';
 
 declare module '@sapphire/pieces' {
 	interface Container {
 		db: PrismaClient;
 		email: Transporter;
+	}
+}
+
+declare module '@skyra/env-utilities' {
+	interface Env {
+		CLIENT_PREFIX: string;
+		NODE_ENV: 'test' | 'development' | 'production';
+		MAIL_SERVER: string;
+		MAIL_PORT: IntegerString;
+		MAIL_USERNAME: string;
+		MAIL_PASSWORD: string;
+		REDIS_HOST: string;
 	}
 }
